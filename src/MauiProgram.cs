994d7fff344e766adf6665950a9312d107af9457
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-09-17 11:39:51 +0200

    fix: load environment variables and add DotNetEnv package

--- src/MauiProgram.cs
+++ src/MauiProgram.cs
@@ -6,6 +6,9 @@ namespace PlotThoseLines
     {
         public static MauiApp CreateMauiApp()
         {
+            // Load environment variables from .env file (located in the build directory)
+            DotNetEnv.Env.Load();
+
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
@@ -16,10 +19,12 @@ namespace PlotThoseLines
 
             builder.Services.AddMauiBlazorWebView();
 
+            string apiKey = Environment.GetEnvironmentVariable("TI_API_KEY") ?? "";
+
             builder.Services.AddScoped(sp => new HttpClient
             {
                 BaseAddress = new Uri("https://api.tokeninsight.com/api/v1/"),
-                DefaultRequestHeaders = { { "TI_API_KEY", Environment.GetEnvironmentVariable("TI_API_KEY") ?? "" } }
+                DefaultRequestHeaders = { { "TI_API_KEY", apiKey } }
             });
 
 #if DEBUG
